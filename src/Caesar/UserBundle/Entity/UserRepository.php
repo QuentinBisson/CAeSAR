fc3c17508460f238644e48eab0f90a21c359a614
David <David@David-PC>
2013-01-24 20:57:31 +0100

    Mise à jour du UserRepository (pas sur que sa marche, le temps que je resolve mon probleme avec la console).

--- src/Caesar/UserBundle/Entity/UserRepository.php
+++ src/Caesar/UserBundle/Entity/UserRepository.php
@@ -3,13 +3,30 @@
 namespace Caesar\UserBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+
 /**
  * Description of UserRepository
  *
  * @author David
  */
-class UserRepository extends EntityRepository{
-    
+class UserRepository extends EntityRepository {
+
+    public function authentificate($login, $password) {
+        $qb = $this->createQueryBuilder('u');
+        $qb->where('u.login = :login')
+                ->setParameter('login', $login)
+                ->andWhere('u.password = :password')
+                ->setParameter(':password', $password);
+        return $qb->getQuery()->getResult();
+    }
+
+    public function identificate($login) {
+        $qb = $this->createQueryBuilder('u');
+        $qb->where('u.login = :login')
+                ->setParameter('login', $login);
+        return $qb->getQuery()->getResult();
+    }
+
 }
 
 ?>
