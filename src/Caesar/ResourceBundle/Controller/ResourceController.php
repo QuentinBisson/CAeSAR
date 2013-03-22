60ed7eb28fa2b328105f859421f3507749f6e721
omegas27 <quentin.bisson@gmail.com>
2013-03-22 02:07:20 +0100

    ajout des traductions pour les emprunts et les retours ainsi que la gestion des messages flashs

--- src/Caesar/ResourceBundle/Controller/ResourceController.php
+++ src/Caesar/ResourceBundle/Controller/ResourceController.php
@@ -62,12 +62,12 @@ class ResourceController extends Controller {
     }
 
     $q = $resource->getQuantity();
-    $borrowingsQuantity = count($resource->getBorrowings());
+    $borrowedQuantity = count($resource->getBorrowings());
 
     //if at least one available
-    if ($q > $borrowingsQuantity) {
+    if ($q > $borrowedQuantity) {
       $reservedQuantity = count($resource->getReservations());
-      $available = $q - $borrowingsQuantity - $reservedQuantity;
+      $available = $q - $borrowedQuantity - $reservedQuantity;
 
       if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
         $user = $this->get('security.context')->getToken()->getUser();
@@ -78,9 +78,12 @@ class ResourceController extends Controller {
           $borrow->setUser($user);
           $em->persist($borrow);
           $em->flush();
-          $message = "La ressource a bien été empruntée.";
+          $this->get('session')->getFlashBag()->add(
+            'notice', $translator->trans('client.borrowing.resource.borrowed', array('%resource%' => $resource->getDescription()))
+          );
+          return $this->redirect($this->generateUrl('caesar_client_homepage'));
         } else {
-          $reservedAvailable = $q - $borrowingsQuantity;
+          $reservedAvailable = $q - $borrowedQuantity;
           $acceptedReservations = array();
           $reservations = $resource->getReservations();
           $i = 0;
@@ -89,14 +92,13 @@ class ResourceController extends Controller {
             array_push($acceptedReservations, $reservations->next());
             $i++;
           }
-
           $reservation = null;
           foreach ($acceptedReservations as $r) {
             if ($r->getUser()->isEqualTo($user)) {
               $reservation = $r;
             }
           }
-          //Si je suis le premier reservateur (sort by date)
+          //Si user parmi les premiers à réserver
           if ($reservation != null) {
             $em->remove($reservation);
             $borrow = new Borrowing();
@@ -104,7 +106,10 @@ class ResourceController extends Controller {
             $borrow->setUser($user);
             $em->persist($borrow);
             $em->flush();
-            $message = "La ressource que vous avez réservée est empruntée.";
+            $this->get('session')->getFlashBag()->add(
+              'notice', $translator->trans('client.borrowing.resource.reservation.borrowed', array('%resource%' => $resource->getDescription()))
+            );
+            return $this->redirect($this->generateUrl('caesar_client_homepage'));
           } else {
             $isInList = false;
             foreach ($resource->getReservations()->toArray() as $reservation) {
@@ -112,24 +117,33 @@ class ResourceController extends Controller {
                 $isInList = true;
               }
             }
+            //Pas le premier sur la liste de réservation
             if ($isInList) {
-              $message = "Malheureusement, des utilisateurs ont réservé cette ressource avant vous.";
-              //Pas le premier sur la liste de réservation
-            } else {
-              $message = "Malheureusement, tous les exemplaires de cette ressource ont été emprunté ou réservé. Il vous est impossible d'emprunter cette ressource.";
-              //Ressource non réservée.
+              $this->get('session')->getFlashBag()->add(
+                'error', $translator->trans('client.borrowing.resource.reservation.error', array('%resource%' => $resource->getDescription()))
+              );
+              return $this->redirect($this->generateUrl('caesar_client_homepage'));
+            } else { //Ressource non réservée.
+              $this->get('session')->getFlashBag()->add(
+                'error', $translator->trans('client.borrowing.resource.reservation.none', array('%resource%' => $resource->getDescription()))
+              );
+              return $this->redirect($this->generateUrl('caesar_client_homepage'));
             }
           }
         }
-      } else {
-        $message = "Veuillez vous connecter avant d'emprunter la ressource.";
-        //Je dois me connecter
+      } else {//Je dois me connecter
+        /* $message = "Veuillez vous connecter avant d'emprunter la ressource.";
+          $this->get('session')->getFlashBag()->add(
+          'info', $translator->trans('client.borrowing.resource.unavailable', array('%resource%' => $resource->getDescription()))
+          );
+          return $this->render('CaesarResourceBundle:Resource:borrow.html.twig', array('resource' => $resource, 'message' => $message)); */
       }
-    } else {
-      $message = "Malheureusement, La ressource que vous souhaitez emprunter n'est plus disponible.";
-      //Plus de ressources disponibles
     }
-    return $this->render('CaesarResourceBundle:Resource:borrow.html.twig', array('resource' => $resource, 'message' => $message));
+    //Plus de ressources disponibles
+    $this->get('session')->getFlashBag()->add(
+      'error', $translator->trans('client.borrowing.resource.unavailable', array('%resource%' => $resource->getDescription()))
+    );
+    return $this->redirect($this->generateUrl('caesar_client_homepage'));
   }
 
   public function returnAction($code) {
@@ -146,32 +160,38 @@ class ResourceController extends Controller {
     }
 
     $q = $resource->getQuantity();
-    $borrowingsQuantity = count($resource->getBorrowings());
+    $borrowedQuantity = count($resource->getBorrowings());
 
-    if ($borrowingsQuantity <= 0) {
-      throw $this->createNotFoundException('Cette ressource ne peut être rendue car elle n\'a pas été emprunté.');
+    if ($borrowedQuantity <= 0) {
+      throw $this->createNotFoundException($translator->trans('client.return.impossible.exception', array('%resource%' => $resource->getDescription())));
     }
 
-    //if only one borrowed
-    if ($borrowingsQuantity == 1) {
-      return new Response('Resource rendu.');
-    } else if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
-      //Rendre
-      $reservedAvailable = $q - $borrowingsQuantity;
-      $acceptedReservations = array();
+    if ($borrowedQuantity == 1) { //if only one borrowed
+      $this->get('session')->getFlashBag()->add(
+        'notice', $translator->trans('client.return.accepted', array('%resource%' => $resource->getDescription()))
+      );
+      return $this->redirect($this->generateUrl('caesar_client_homepage'));
+    } else if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) { //Rendre
+      $reservedAvailable = $q - $borrowedQuantity;
+      $reservationsToNotify = array();
       $reservations = $resource->getReservations();
       $i = 0;
-      array_push($acceptedReservations, $reservations->first());
+      array_push($reservationsToNotify, $reservations->first());
       while ($i < $reservedAvailable - 1) {
-        array_push($acceptedReservations, $reservations->next());
+        array_push($reservationsToNotify, $reservations->next());
         $i++;
       }
-      //send email au reservateurs acceptés
-    } else {
-      //Je dois me connecter
+      //TODO send email to $acceptedReservations
+      $this->get('session')->getFlashBag()->add(
+        'notice', $translator->trans('client.return.accepted', array('%resource%' => $resource->getDescription()))
+      );
     }
 
-    return $this->render('CaesarResourceBundle:Resource:return.html.twig', array('resource' => $resource));
+    /* $message = "Veuillez vous connecter avant d'emprunter la ressource.";
+      $this->get('session')->getFlashBag()->add(
+      'info', $translator->trans('client.borrowing.resource.unavailable', array('%resource%' => $resource->getDescription()))
+      );
+      return $this->render('CaesarResourceBundle:Resource:return.html.twig', array('resource' => $resource)); */
   }
 
 }
