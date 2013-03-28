09a359e063e7631ec9f223f74a4df25d91038270
David <David@David-PC>
2013-03-28 15:14:03 +0100

    Modif design login Ajout test

--- src/Caesar/AdminBundle/Controller/SecurityController.php
+++ src/Caesar/AdminBundle/Controller/SecurityController.php
@@ -8,12 +8,12 @@ use Caesar\AdminBundle\Form\LoginType;
 use Caesar\AdminBundle\Form\LoginHandler;
 
 class SecurityController extends Controller {
-    
+
     public function loginAction() {
         $form = $this->createForm(new LoginType());
         return $this->render('CaesarAdminBundle:Security:login.html.twig', array(
-            'form' => $form->createView()
-        ));
+                    'form' => $form->createView()
+                ));
     }
-    
+
 }
